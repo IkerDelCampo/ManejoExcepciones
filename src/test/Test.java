fc3ed8dcd816217baffce6ed2228fa42d53a46e3
IkerDelCampo <rekidelcampo@gmail.com>
2021-10-15 12:23:21 +0200

    Initial commit

---
+++ src/test/Test.java
@@ -0,0 +1,17 @@
+
+package test;
+
+/*
+ */
+public class Test {
+    public static void main(String[] args) {
+        int res = 0;
+        
+        try {
+            res= 10/0;
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+        
+    }
+}
